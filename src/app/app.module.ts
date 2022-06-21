f30f873a8578a7e9d78d8409b8eb6a65fd5bcaf3
jan1ooo <filhojanio67@gmail.com>
2022-06-20 22:31:36 -0300

    End

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { CoreModule } from './core/core.module';
+import { CourseModule } from './courses/course.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
